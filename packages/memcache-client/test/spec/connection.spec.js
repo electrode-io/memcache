33340cf1bb0596760716dd736417c403405e57da
Joel Chen <xchen@walmartlabs.com>
2017-04-18 16:14:43 -0700

    memcache-client: add tests for connection

--- packages/memcache-client/test/spec/connection.spec.js
+++ packages/memcache-client/test/spec/connection.spec.js
@@ -1,3 +1,26 @@
-describe("connection", function () {
+"use strict";
 
+const Connection = require("../../lib/connection");
+
+describe("connection", function () {
+  it("waitReady should self if ready flag is true", () => {
+    const x = new Connection({ socketID: 1 });
+    x.ready = true;
+    expect(x.waitReady().isFulfilled()).to.be.true;
+  });
+
+  it("cmdAction_ERROR should append message after command", () => {
+    const x = new Connection({ socketID: 1 });
+    let testError;
+    x.queueCommand({
+      callback: (error) => (testError = error)
+    });
+    x.cmdAction_ERROR(["test", "hello", "world"]);
+    expect(testError.message).to.equal("test hello world");
+  });
+
+  it("cmdAction_undefined should return false for unknown command", () => {
+    const x = new Connection({ socketID: 1 });
+    expect(x.cmdAction_undefined(["test"])).to.be.false;
+  });
 });
